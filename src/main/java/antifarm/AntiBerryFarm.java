dddcf85a89fbd9f78ad1c4789b7742c44ea9cebe
sillycorn <untesha04+sillycorn@gmail.com>
2026-04-11 16:13:57 +0300

    + fixed antilavafarm not working + improvements to antiberryfarm

--- src/main/java/antifarm/AntiBerryFarm.java
+++ src/main/java/antifarm/AntiBerryFarm.java
@@ -1,7 +1,12 @@
 package antifarm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -15,36 +20,40 @@ import configuration.Configuration;
 import core.AntiFarmPlugin;
 
 public class AntiBerryFarm implements Listener {
+    private final AntiFarmPlugin plugin;
+    private boolean preventBerryFarms;
+    private Set<String> disabledWorlds;
+    private static final ItemStack BERRY_TEMPLATE = new ItemStack(Material.SWEET_BERRIES, 1);
 
-	private final Configuration config;
+    public AntiBerryFarm(AntiFarmPlugin plugin) {
+        this.plugin = plugin;
+        reloadConf();
+    }
 
-	public AntiBerryFarm(AntiFarmPlugin plugin) {
-		this.config = plugin.getConfig();
-	}
+    public void reloadConf() {
+        Configuration config = plugin.getConfig();
+        this.preventBerryFarms = config.getBoolean("farms-settings.prevent-berry-farms", true);
+        this.disabledWorlds = new HashSet<>(config.getStringList("settings.disabled-worlds"));
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	private void onEntityChangeBlock(EntityChangeBlockEvent event) {
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        if (!preventBerryFarms) return;
+        if (event.getEntityType() != EntityType.FOX) return;
 
-		if (config.getStringList("settings.disabled-worlds").contains(event.getBlock().getWorld().getName())) return;
+        Block block = event.getBlock();
+        if (block.getType() != Material.SWEET_BERRY_BUSH) return;
 
-		if (event.isCancelled() || event.getBlock() == null || event.getEntity() == null) return;
-		if (!event.getEntity().getType().equals(EntityType.FOX)) return;
-		if (!event.getBlock().getType().equals(Material.SWEET_BERRY_BUSH)) return;
-		if (!config.getBoolean("farms-settings.prevent-berry-farms", true)) return;
-
-		event.setCancelled(true);
-
-		event.getEntity().getWorld().playSound(event.getEntity(), Sound.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, 1.0f, 1.0f);
-		Ageable sweetBerryBush = (Ageable) event.getBlock().getBlockData();
-		sweetBerryBush.setAge(1);
-		event.getBlock().setBlockData(sweetBerryBush);
-
-		LivingEntity fox = (LivingEntity) event.getEntity();
-
-		if (!fox.getEquipment().getItemInMainHand().getType().equals(Material.AIR)) return;
-
-		fox.getEquipment().setItemInMainHand(new ItemStack(Material.SWEET_BERRIES, 1));
-
-	}
-
+        World world = block.getWorld();
+        if (!disabledWorlds.isEmpty() && disabledWorlds.contains(world.getName())) return;
+        event.setCancelled(true);
+        LivingEntity fox = (LivingEntity) event.getEntity();
+        Ageable sweetBerryBush = (Ageable) block.getBlockData();
+        sweetBerryBush.setAge(1);
+        block.setBlockData(sweetBerryBush);
+        if (fox.getEquipment() != null && fox.getEquipment().getItemInMainHand().getType() == Material.AIR) {
+            fox.getEquipment().setItemInMainHand(BERRY_TEMPLATE.clone());
+            world.playSound(fox.getLocation(), Sound.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, 1.0f, 1.0f);
+        }
+    }
 }
